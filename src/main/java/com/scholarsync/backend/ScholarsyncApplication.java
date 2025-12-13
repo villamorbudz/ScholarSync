0b0a30d63ed16299a8d92e3e36896076a6a9bb12
Giles Anthony Villamor II <villamorbudz@gmail.com>
2025-12-13 19:48:23 +0800

    initial commit

---
+++ src/main/java/com/scholarsync/backend/ScholarsyncApplication.java
@@ -0,0 +1,13 @@
+package com.scholarsync.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholarsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScholarsyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scholarsync/backend/ScholarsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scholarsync.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScholarsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
